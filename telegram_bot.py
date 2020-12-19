46d9b3c3399443d4e3fb61b6354334c91c1ac349
Shivani-Pulluru <72390153+Shivani-Pulluru@users.noreply.github.com>
2020-12-19 19:29:47 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,44 +1,33 @@
-from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
-from Adafruit_IO import Client,Data,Feed
-import os
-def Turnoff(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned off")
-  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/7501809_preview.png')
-  send_value(0)
-def Turnon(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned on")
-  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/507790_preview.png')
-  send_value(1)
-def send_value(value):
-  feed = aio.feeds('lc')
-  aio.send_data(feed.key,value)
+!pip3 install adafruit.io
+from Adafruit_IO import RequestError, Client, Feed
+from Adafruit_IO import Data
+username = "Shivani_Pulluru"                   
+code = "aio_CByY93NqbkAZqnFP938Svaup22Wc"   
+aio = Client(username,code)
+!pip install python-telegram-bot
 
-def input_message(update, context):
-  text=update.message.text.upper()
-  text=update.message.text
-  if text == 'Turn on':
-    send_value(1)
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Light turned on")
-    context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/507790_preview.png')
-  elif text == 'Turn off':
-    send_value(0)
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Light turned off")
-    context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/7501809_preview.png')
-def start(update,context):
-  start_message='''
-/Turnoff or 'Turn off':To turn off the light 
-/Turnon or 'Turn on'  :To turn on the light 
-'''
-  context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
-ADAFRUIT_IO_USERNAME = os.getenv('ADAFRUIT_IO_USERNAME')
-ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY')
-TOKEN = os.getenv('TOKEN')
-aio = Client(ADAFRUIT_IO_USERNAME,ADAFRUIT_IO_KEY)
-updater=Updater(TOKEN,use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('turnoff',Turnoff))
-dispatcher.add_handler(CommandHandler('turnon',Turnon))
-dispatcher.add_handler(CommandHandler('start',start))
-dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command),input_message))
-updater.start_polling()
-updater.idle()
+def on(bot,update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    txt = 'light is turning on'
+    pic = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Green_sphere.svg/1024px-Green_sphere.svg.png'
+    bot.send_message(chat_id,txt)
+    bot.send_photo(chat_id,pic)
+    value = Data(value=1)
+    value_send = aio.create_data('lit',value)
+    
+def off(bot,update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    txt = 'light is turning off'
+    pic = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuueKIndqjMG0rlzPZrO0UUFP6ts8b_CrUIQ&usqp=CAU'
+    bot.send_message(chat_id,txt)
+    bot.send_photo(chat_id,pic)
+    value = Data(value=0)
+    value_send = aio.create_data('lit',value)
+    u = Updater('1409120167:AAGu_pP6ntFkck-Y7FtxBkbLBrIqQmQiu5M')  
+dp = u.dispatcher
+dp.add_handler(CommandHandler('on',on))
+dp.add_handler(CommandHandler('off',off))
+u.start_polling()
+u.idle()  
